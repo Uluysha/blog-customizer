14a17a4fc6a649a821cb42fb565092b444303ec7
Uliana Ukolova <ufilippovaa18@yandex.ru>
2025-10-04 00:43:40 +0400

    first try

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useEffect, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,20 +15,65 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+export const ARTICLE_STATE_KEY = 'articleState';
+
+export const saveArticleState = (state: ArticleStateType) => {
+	try {
+		localStorage.setItem(ARTICLE_STATE_KEY, JSON.stringify(state));
+	} catch (error) {
+		console.error('Ошибка установки состояния: ', error);
+	}
+};
+
+export const getStorageArticleState = (): ArticleStateType => {
+	try {
+		const storageState = localStorage.getItem(ARTICLE_STATE_KEY);
+		if (storageState) {
+			return JSON.parse(storageState);
+		}
+	} catch (error) {
+		console.error('Ошибка загрузки состояния: ', error);
+	}
+	return defaultArticleState;
+};
+
 const App = () => {
+	const storageState = getStorageArticleState();
+	const [articleState, setArticleState] =
+		useState<ArticleStateType>(storageState);
+
+	useEffect(() => {
+		setArticleState(getStorageArticleState());
+	}, []);
+
+	useEffect(() => {
+		saveArticleState(articleState);
+	}, [articleState]);
+
+	const onArticleStateChangeHandler = (changedState: ArticleStateType) => {
+		setArticleState(changedState);
+	};
+
+	const onArticleStateResetHandler = () => {
+		setArticleState(defaultArticleState);
+	};
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleState.fontFamilyOption.value,
+					'--font-size': articleState.fontSizeOption.value,
+					'--font-color': articleState.fontColor.value,
+					'--container-width': articleState.contentWidth.value,
+					'--bg-color': articleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				articleState={articleState}
+				onArticleStateChange={onArticleStateChangeHandler}
+				onArticleStateReset={onArticleStateResetHandler}
+			/>
 			<Article />
 		</main>
 	);
